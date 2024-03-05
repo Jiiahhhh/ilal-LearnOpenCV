9ad45a99cda4fbe78e0228aa994f545448be1ab5
DESKTOP-I450EA2\ijlal <ijlalnurhadi01@gmail.com>
2024-03-05 23:45:45 +0800

    update

--- 02-Advanced/bitwise.py
+++ 02-Advanced/bitwise.py
@@ -1,23 +1,24 @@
 import cv2 as cv
 import numpy as np
+#biasanya dipakai untuk proses segmentasi objek/masking
 
 blank = np.zeros((400, 400), dtype='uint8')
 
-rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
-circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1)
+rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1) #buat kotak
+circle = cv.circle(blank.copy(), (200, 200), 200, 255, -1) #buat lingkaran
 
 cv.imshow('Rectangle', rectangle)
 cv.imshow('Circle', circle)
 
-#bitwise AND --> interscting regions
+#bitwise AND --> interscting regions (pixel yang saling bertemu saja yang akan muncul)
 bitwise_and = cv.bitwise_and(rectangle, circle)
 cv.imshow('Bitwise AND', bitwise_and)
 
-#bitwise OR --> non-intersecting and intersecting regions
+#bitwise OR --> non-intersecting and intersecting regions (semua pixel yang ada akan muncul)
 bitwise_or = cv.bitwise_or(rectangle, circle)
 cv.imshow('Bitwise OR', bitwise_or)
 
-#bitwise XOR -> non-intersecting regions
+#bitwise XOR -> non-intersecting regions (semua pixel yang tidak bertemu saja yang akan muncul)
 bitwise_xor = cv.bitwise_xor(rectangle, circle)
 cv.imshow('Bitwise XOR', bitwise_xor)
 
--- 02-Advanced/masking.py
+++ 02-Advanced/masking.py
@@ -11,6 +11,8 @@ cv.imshow('Blank Image', blank)
 circle = cv.circle(blank.copy(), (img.shape[1]//2 + 45, img.shape[0]//2), 100, 255, -1)
 
 rectangle = cv.rectangle(blank.copy(), (30, 30), (370, 370), 255, -1)
+img_or = cv.bitwise_or(circle, rectangle)
+cv.imshow('or', img_or)
 
 weird_shape = cv.bitwise_and(circle, rectangle)
 cv.imshow('Weird Shape', weird_shape)
